c0ded2596e89679862d49109fe960a0064ba274c
gtucker314 <gtucker314@gmail.com>
2024-07-01 23:38:38 -0700

    Add files via upload

---
+++ project.js
@@ -0,0 +1,25 @@
+const width = 125;
+const height = 125;
+
+setDocDimensions(width, height);
+
+const finalLines = [];
+
+
+function createCircle(radius) {
+  const t = new bt.Turtle()
+  t.arc(360, 5)
+  console.log("circle")
+  return t.path
+}
+
+// let randomShape = new bt.randIntInRange (1,2)
+//let shape = []
+//if (randomShape == 1) {
+//shape = createCircle(bt.randIntInRange(10, 50)
+}
+//else {
+//shape = createRec(bt.randIntInRange(10,50)
+}
+
+//return shape 
